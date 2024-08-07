436fea5972be2c1a7b6335da15654447493e8f3c
Gean Batista <geanbatista.001@gmail.com>
2024-08-07 10:00:37 -0300

    Add files via upload

---
+++ GrupoDaSara-main/programação git/teste.py
@@ -0,0 +1,26 @@
+#Mostra a frase abaixo
+
+
+""" 
+Mostra 
+a 
+frase
+abaixo 
+"""
+
+
+print("Bem-Vindo")
+name = input("qual seu nome?")
+print("Bem-Vindo,", name + "!")
+
+print("Em que posso te ajudar?")
+print("1-Queijo")
+print("2-Água")
+print("3-Leite")
+print("4-Pão")
+
+opcao = int(input("Digite a opção desejada")) 
+
+if opcao == 1:
+    print
+    
